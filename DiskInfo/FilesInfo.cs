00c84ff50ff14498f77936e4ab7286482cdb15a0
Pixelgon <mm@pixelgon.cz>
2023-04-02 21:09:37 +0200

    Initial commit

---
+++ DiskInfo/FilesInfo.cs
@@ -0,0 +1,70 @@
+﻿namespace DiskInfo;
+
+public static class FilesInfo
+{
+    public static string[] GetDisks()
+    {
+        return DriveInfo.GetDrives().Select(x => x.Name).ToArray();
+    }
+    
+    public static string[] GetFiles(string path)
+    {
+        return Directory.GetFiles(path);
+    }
+    
+    public static string[] GetFilesByExtension(string path, string extension)
+    {
+        return Directory.GetFiles(path, extension);
+    }
+    
+    public static string[] GetAllFiles(string path)
+    {
+        return Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+    }
+    
+    public static double GetFolderSize(string path)
+    {
+       return (double)new DirectoryInfo(path).EnumerateFiles("*").Sum(x => x.Length) * 1e-9;
+    }
+    
+    public static double GetFolderSizeAll(string path)
+    {
+        return (double)new DirectoryInfo(path).EnumerateFiles("*", SearchOption.AllDirectories).Sum(x => x.Length) * 1e-9;
+    }
+    
+    public static int GetFolderCount(string path)
+    {
+        return (int)new DirectoryInfo(path).EnumerateFiles("*", SearchOption.AllDirectories).Count();
+    }
+    
+    public static void PrintDirectoryTree(string path, string prefix = "")
+    {
+        Console.WriteLine(prefix + Path.GetFileName(path) + "/");
+        prefix += "  ";
+
+        try
+        {
+            foreach (string subPath in Directory.GetDirectories(path))
+            {
+                PrintDirectoryTree(subPath, prefix);
+            }
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine(prefix + "Access to this directory is denied.");
+        }
+
+        try
+        {
+            foreach (string file in Directory.GetFiles(path))
+            {
+                Console.WriteLine(prefix + Path.GetFileName(file));
+            }
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine(prefix + "Access to the files in this directory is denied.");
+        }
+    }
+
+}
---
+++ DiskInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DiskInfo;
+
+
+//main class
+class Program
+{
+    static void Main(string[] args)
+    {
+       FilesInfo.PrintDirectoryTree("C:\\users\\pixel\\downloads");
+    }
+}
+    
